b31597d6bd50b7050fedbe1bbc9e7588bafabdd2
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-07-10 14:49:45 +0800

    Added Modal for New Book

---
+++ library.js
@@ -0,0 +1,23 @@
+// MODAL 
+var modal = document.getElementById("newBookModal");
+
+var btn = document.getElementById("toggleModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//
